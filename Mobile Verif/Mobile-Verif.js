16cd24b9f6d513bb4c2f333081e98c53a0a9c9cd
JaisonST <jaisonmanu@gmail.com>
2021-06-25 23:03:09 +0400

    fixed apis

--- Mobile Verif/Mobile-Verif.js
+++ Mobile Verif/Mobile-Verif.js
@@ -1,8 +1,9 @@
+const port = 8080; 
+const hostname = 'localhost';
+
+
 // Load Dependencies
 var express = require('express');
-var exphbs  = require('express-handlebars');
-var bodyParser = require('body-parser');
-
 // Load configuration from .env file
 require('dotenv').config();
 
@@ -10,43 +11,33 @@ require('dotenv').config();
 var messagebird = require('messagebird')('TQWMwfiH3bHO1wRhDCrarjF8d');
 
 // Set up and configure the Express framework
-var app = express();
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+const app = express();
 app.use(bodyParser.urlencoded({ extended : true }));
 
-// Display page to ask the user for their phone number
-app.get('/', function(req, res) {
-    res.render('step1');
-});
 
 // Handle phone number submission
-app.post('/step2', function(req, res) {
+app.post('/api/phone/msgGen', function(req, res) {
     var number = req.body.number;
     
     // Make request to Verify API
     messagebird.verify.create(number, {
-        originator : 'Code',
-        template : 'Your verification code is %token.'
+        // originator : 'Code',
+        // template : 'Your verification code is %token.'
     }, function (err, response) {
         if (err) {
             // Request has failed
             console.log(err);
-            res.render('step1', {
-                error : err.errors[0].description
-            });
+            res.json({status:400, error: true,msg: err.errors[0].description});
         } else {
             // Request was successful
             console.log(response);
-            res.render('step2', {
-                id : response.id
-            });
+            res.render({status:200, error: false, msgSent: true});
         }
     })    
 });
 
 // Verify whether the token is correct
-app.post('/step3', function(req, res) {
+app.post('/api/phone/verifyCode', function(req, res) {
     var id = req.body.id;
     var token = req.body.token;
 
@@ -55,17 +46,16 @@ app.post('/step3', function(req, res) {
         if (err) {
             // Verification has failed
             console.log(err);
-            res.render('step2', {
-                error: err.errors[0].description,
-                id : id
-            });
+            res.render({status:400, error:true, msg: err.errors[0].description,});
         } else {
             // Verification was successful
             console.log(response);
-            res.render('step3');
+            res.render({status:200, error: false, codeVerified: true});
         }
     })    
 });
 
 // Start the application
-app.listen(8080);
+app.listen(port, hostname, ()=> {
+    console.log(`Listneing to port at http://${hostname}:${port}/api/phone`);
+});
